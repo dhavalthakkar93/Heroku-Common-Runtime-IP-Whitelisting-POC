535ed971d0b7d717d9bd260d6fbdfc4c3f59e4b1
Dhaval Thakkar <dhavalthakkar4995@gmail.com>
2020-01-29 21:34:56 +0530

    init

---
+++ server.js
@@ -0,0 +1,26 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', function(req, res) {            
+    res.send('Hello World!');
+})
+
+app.post('/ping', function(req, res) {
+    res.send(req.body);    
+})
+
+app.set('port', 3000);
+
+var server = app.listen(app.get('port'), function () {
+    if (process.env.DYNO) {
+        console.log('Running on Heroku...');
+        fs.openSync('/tmp/app-initialized', 'w');
+      }
+    console.log('Node app is running on port:', app.get('port'));
+});
